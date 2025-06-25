16857a8dda3068abf14019b26e64b5f3f210172a
tulsyanshubham <tulsyanshubham3639@gmail.com>
2025-06-25 16:09:50 +0530

    remove homepage and gh-pages dependency from package.json; update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/photo-editor',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
